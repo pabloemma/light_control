efb079576e042652b86dc3e18a36eb7dc8e2f98d
andi klein <pabloemma@gmail.com>
2026-04-26 13:06:23 +0200

    ikea_class

---
+++ src/ikea_class.py
@@ -0,0 +1,33 @@
+# class to tunr the ikea outlest on and off
+#very simple code
+
+import subprocess
+
+
+class IkeaControl(object):
+
+    def __init__(self, host=None, username=None, password=None):
+        self.host = host
+        self.username = username
+        if password is None:
+            f = open("config/light.txt")
+
+            self.password = f.read().strip()  
+            f.close()  
+        else:
+            self.password = password
+
+    def turn_on(self, device):
+        command = f'mosquitto_pub -h {self.host} -t \'zigbee2mqtt/{device}/set\' -m "ON" -d -u \'{self.username}\' -P \'{self.password}\''
+        subprocess.Popen(command, shell=True)
+
+    def turn_off(self, device):
+        command = f'mosquitto_pub -h {self.host} -t \'zigbee2mqtt/{device}/set\' -m "OFF" -d -u \'{self.username}\' -P \'{self.password}\''
+        subprocess.Popen(command, shell=True)
+
+if __name__ == "__main__":
+    host = '192.168.3.201'
+    username = 'addons'
+    IKC = IkeaControl( host = host,username=username)
+    IKC.turn_on('ikea_5')
+    IKC.turn_off('ikea_5')
