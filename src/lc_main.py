3c316e40f9b49dc030069e790fc637dc4dd45039
andi klein <pabloemma@gmail.com>
2026-04-26 14:52:55 +0200

    more work and new code lc_control

--- src/lc_main.py
+++ src/lc_main.py
@@ -1,13 +1,33 @@
 # light control_main ist the main control for the random light at a house
 # it will be connected to a push button to turn it on and off
 #there will be a list of how many toime windows and when
-# which will be controlled thorugh a config json filer
+# which will be controlled thorugh a config json filerurrently there are 6 ikea outlets defined in the config file;
+#however I have only 5, #3 does not exist
+# A switch is used in the system if it has an action eithe ON or OFF.
+#if the action is blank then that switch is not being used.
+# There is a start and end time and a window. If the widonw is 0, it means to use the exact start and end time
+# if the winodw has a value this is used to set a start time and endtime randomly within that window
+# as an example: starttime = 18:00, window :60 will mean that the start time will be somewhere between 17:30 and 18:30 and the same for the
+# end time. This is to make the light more random and less predictable.
+# the loop time is in munutes and is used to determine how often the system checks the time and the actions. The system will check every loop time if any of the actions need to be performed.
+# NOTE: Currently the system has only two actions either OFF or ON. The beavior is that the action selected is valid for the chosen time windwo.
+#
+# The system controls throgh zigbee2mqtt and mosquitto_pub, the ikea outlets. The system is designed to be run on a raspberry pi, 
+# but it can be run on any system that has python and the required libraries installed. However it communicates with a MQTT dongle, in my case
+# connected to a raspberry pi 4 which is running home assistant
+# the system is designed to be run as a service on the raspberry pi, but it can also be run as a standalone application.
+
 
 #regular imports
 import os
 import sys
 import platform
 from loguru import logger
+
+# my imports
+import lc_config as LC
+import ikea_class as IKC    
+
 # globals
 
 
@@ -59,22 +79,23 @@ class lc_main(QMainWindow):
         self.config_file = config_file
 
         # default config_file
-        if(self.config_file == None or not os.path.isfile(self.config_file)):
+        if(self.config_file == None) :
             #give default name
-            self.config_file = self.GetRootDir()+'config/lc_control.json'
- 
+  
+
+
             try:
-                # get configuration
-                self.SetupConfig
-            except:
                 logger.warning("error in configuration, opening dialog")
                 self.config_file , filter = QFileDialog.getOpenFileName(self,
                                 self.tr("Open Config file"), "~", self.tr("*.json"))
+                self.myconf = LC.lc_config(config_file = self.config_file)
 
-
- 
+            except Exception as e:
+                logger.error(f"Error loading configuration file: {e}")
+                sys.exit(1)
+        else:
+            self.myconf = LC.lc_config(config_file = self.config_file)
             
-
 
     def CloseApp(self):
         logger.info("closing down")
