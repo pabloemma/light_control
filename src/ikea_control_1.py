2661e77dbd82f8006eba73abeb1f8b0d2744a949
andi klein <pabloemma@gmail.com>
2026-04-27 15:38:30 +0200

    *

---
+++ src/ikea_control_1.py
@@ -0,0 +1,24 @@
+import subprocess
+
+a = 'mosquitto_pub -h 192.168.3.201  -t '
+
+
+#b0 = 'ikea_5'
+b0 = 'ikea_5'
+
+#b = '\'zigbee2mqtt/'+b0+'/set\' '
+b = '\'zigbee2mqtt/'+b0 +'\' '
+
+c = '-m \"ON" '
+
+c0 = '-m \"OFF" '
+
+d =  ' -d -u \'addons\' -P \'phooGhu0au4zaem3ooB5yapheM1oXaifishiubooH0quio2Ziig2OorohC1oShen\' '
+
+
+
+e = a + b + c0 + d
+
+
+
+subprocess.Popen(e,shell=True)
