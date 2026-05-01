1b1a436756970c06e3c82b2395afdd005aca796b
andi klein <pabloemma@gmail.com>
2026-05-01 09:34:53 +0200

    typo

--- src/ikea_class.py
+++ src/ikea_class.py
@@ -10,7 +10,7 @@ class IkeaControl(object):
         self.host = host
         self.username = username
         if password is None:
-            f = open("/home/klein/git/light_controlconfig/light.txt")
+            f = open("/home/klein/git/light_control/config/light.txt")
 
             self.password = f.read().strip()  
             f.close()  
